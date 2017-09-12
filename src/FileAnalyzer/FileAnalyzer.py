fac4c32a4197506bca8ee55e56ccc927110811f6
guillemnicolau <guillemnicolau@gmail.com>
2017-09-12 14:50:37 +0200

    Now the application works, user interface added, 4 different plots added

--- src/FileAnalyzer/FileAnalyzer.py
+++ src/FileAnalyzer/FileAnalyzer.py
@@ -1,14 +1,101 @@
+import re
+import pylab as pl
+import numpy as np
 
 
+_date_line = re.compile('[0-9]+/[0-9]+/[0-9]+')
+_message_line = re.compile('[0-9][0-9]:[0-9][0-9] - [\w0-9:+, ]+')
 
 
 class FileAnalyzer:
 
     def __init__(self, input_data):
+        """
+        File Analyzer initializer
+        :param input_data: data in input file
+        """
         self.data = input_data
+        self.messages_day = {}
+        self.messages_hours = {}
+        self.messages_user = {}
+        self.messages_user_chars = {}
 
-    def process_input(self): 
+    def plot_messages_days(self):
+        """
+        This method plots the number of messages sent per day
+        :return:
+        """
+        x = np.arange(len(self.messages_day))
+        pl.bar(x, self.messages_day.values(), align='center', width=0.5)
+        pl.xticks(x, self.messages_day.keys(), rotation=45)
+        ymax = max(self.messages_day.values()) + 1
+        pl.ylim(0, ymax)
+        pl.show()
+
+    def plot_messages_hours(self):
+        """
+        This method plots the total number of messages sent per hour in any day
+        :return:
+        """
+        lists = sorted(self.messages_hours.items())
+        x, y = zip(*lists)
+        pl.plot(x, y)
+        pl.show()
+
+    def plot_messages_user(self):
+        """
+        This method plots the number of messages sent per user
+        :return:
+        """
+        x = np.arange(len(self.messages_user))
+        pl.bar(x, self.messages_user.values(), align='center', width=0.5)
+        pl.xticks(x, self.messages_user.keys(), rotation=45)
+        ymax = max(self.messages_user.values()) + 1
+        pl.ylim(0, ymax)
+        # pl.savefig('foo.png')
+        pl.show()
+
+    def plot_messages_user_chars(self):
+        """
+        This method plots the number of chars from all messages sent per user
+        :return:
+        """
+        x = np.arange(len(self.messages_user_chars))
+        pl.bar(x, self.messages_user_chars.values(), align='center', width=0.5)
+        pl.xticks(x, self.messages_user_chars.keys(), rotation=45)
+        ymax = max(self.messages_user_chars.values()) + 1
+        pl.ylim(0, ymax)
+        pl.show()
+
+    def process_input(self):
+        """
+        This method processes the input data and accumulates all interesting
+        information in order to be able to plot it afterwards
+        :return:
+        """
         for row in self.data:
             lines = row.split('\n')
             for line in lines:
-                print "line:", line
+                m = _date_line.match(line)
+                if m is not None:
+                    self.messages_day[line] = self.messages_day.get(line, 0) + 1
+                else:
+                    if 'El codi de seguretat de ' not in line and \
+                       ' ha afegit ' not in line and \
+                       ' marxa' not in line and \
+                       'a aquest grup ara estan assegurats amb' not in line and \
+                       'ha creat el grup' not in line and \
+                       'Has canviat ' not in line and \
+                       'ha canviat ' not in line and \
+                       ' expulsat ' not in line and \
+                       'aquest xat i trucades ara estan assegurats' not in line:
+                        n = _message_line.match(line)
+                        if n is not None:
+                            hour = line.split(':')[0]
+                            self.messages_hours[hour] = self.messages_hours.get(hour, 0) + 1
+                            user = line.split(':')[1].split(' - ')[1]
+                            if '(' in user:
+                                user = user.split('(')[0]
+                            self.messages_user[user] = self.messages_user.get(user, 0) + 1
+                            message = line.split(':')[-1]
+                            self.messages_user_chars[user] = self.messages_user_chars.get(user, 0) + len(message)
--- src/whatsapp_file_processor.py
+++ src/whatsapp_file_processor.py
@@ -7,32 +7,100 @@ def clean_screen():
     print(chr(27) + "[2J")
 
 
-
-# Print method for the first message
-def presentation():
+def message_header():
+    """
+    Print method for the header of the application
+    :return:
+    """
     print "-------------------------- Welcome to the Whatsapp Conversation analyzer app --------------------------\n" \
           "The default path to the data file is this one:\n" \
           "       data/input.txt\n" \
           "If you would like to use another data file, insert the path to it as a parameter of the application.\n"
 
 
-# Method used to read the input_data
-def process_input(input_path):
-    with open(input_path, 'r') as f:
+def process_input(input_file_path):
+    """
+    # Method used to read the input_data
+    :param input_file_path: path to the input file
+    :return:
+    """
+    with open(input_file_path, 'r') as f:
         read_data = f.read()
     return read_data.split(', ')
 
 
+def message_output():
+    """
+    Print method for all the available modes
+    :return:
+    """
+    show_modes()
+    print " HELP     - Show the initial application message\n" + \
+          " MODES    - Show the available modes\n" + \
+          " EXIT     - Quit application"
+
+
+def show_modes():
+    """
+    Print method for the available data processing modes
+    :return:
+    """
+    print "Available modes:"
+    print " 1 - Get Pages by Score Ranking\n" + \
+          " 2 - Get Pages by Comment Ranking\n" + \
+          " 3 - Get Users by Submissions Score Ranking\n" + \
+          " 4 - Get Users by Submissions Quantity"
+
+
+def check_input(input_var):
+    """
+    Method to check if the selected method is correct, and to exit if wanted
+    :param input_var: user mode selected
+    :return: True (Mode to execute) / False (Mode executed or incorrect)
+    """
+    if input_var.lower() == 'exit':
+        print "The application will now end."
+        clean_screen()
+        raise SystemExit
+    elif input_var.lower() == 'modes':
+        show_modes()
+        return False
+    elif input_var.lower() == 'help':
+        message_output()
+        return False
+    elif input_var not in ['1', '2', '3', '4']:
+        print "Please enter a valid mode."
+        return False
+    return True
+
 if __name__ == '__main__':
 
     clean_screen()
-    presentation()
+    message_output()
 
     if len(sys.argv) == 1:
-        input_path = 'data/input.txt'
+        input_path = '/home/guillemnicolau/WhatsappStatistics/data/input.txt'
     else:
         input_path = sys.argv[1]
     input_data = process_input(input_path)
 
     my_file_analyzer = FileAnalyzer(input_data)
     my_file_analyzer.process_input()
+
+    message_header()
+    message_output()
+
+    while True:
+        correct_input = False
+        while not correct_input:
+            print "***************************"
+            var = raw_input("Please, enter a new mode: ")
+            correct_input = check_input(var)
+        if var == '1':
+            my_file_analyzer.plot_messages_days()
+        elif var == '2':
+            my_file_analyzer.plot_messages_hours()
+        elif var == '3':
+            my_file_analyzer.plot_messages_user()
+        elif var == '4':
+            my_file_analyzer.plot_messages_user_chars()
